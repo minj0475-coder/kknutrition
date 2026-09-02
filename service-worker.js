55d0c9b622f0dd4c61dcb143a8c75563c92cf05c
minj0475-coder <minj0475@gmail.com>
2026-09-02 09:57:14 +0900

    급식소위원회 표 이중 외곽선 제거

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = 'kknutrition-pwa-v173';
+const CACHE_NAME = 'kknutrition-pwa-v174';
 const CORE_ASSETS = [
   './',
   './index.html',
   './school-lunch-tv.html?v=20260831_021',
-  './assets/site_v2.css?v=20260902_010',
+  './assets/site_v2.css?v=20260902_011',
   './assets/auth.js?v=20260710_001',
   './assets/data-guard.js?v=20260902_001',
   './assets/bookmarks.js?v=20260713_004',
