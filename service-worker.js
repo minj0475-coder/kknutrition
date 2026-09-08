a0bd845fa647c9e2a9dd3ca6c01e34cab1b89045
minj0475-coder <minj0475@gmail.com>
2026-09-09 08:13:11 +0900

    Improve responsive vendor contact layouts

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = 'kknutrition-pwa-v207';
+const CACHE_NAME = 'kknutrition-pwa-v208';
 const CORE_ASSETS = [
   './',
   './index.html',
   './school-lunch-tv.html?v=20260908_031',
-  './assets/site_v2.css?v=20260909_001',
+  './assets/site_v2.css?v=20260909_002',
   './assets/auth.js?v=20260710_001',
   './assets/data-guard.js?v=20260902_001',
   './assets/bookmarks.js?v=20260908_006',
