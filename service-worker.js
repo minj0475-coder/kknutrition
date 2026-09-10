d780dd85d4aaf65260b77c590db11ad6c2d05577
minj0475-coder <minj0475@gmail.com>
2026-09-10 12:04:13 +0900

    style: flatten complaint add icon

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = 'kknutrition-pwa-v235';
+const CACHE_NAME = 'kknutrition-pwa-v236';
 const CORE_ASSETS = [
   './',
   './index.html',
   './school-lunch-tv.html?v=20260909_009',
-  './assets/site_v2.css?v=20260910_007',
+  './assets/site_v2.css?v=20260910_008',
   './assets/auth.js?v=20260710_001',
   './assets/data-guard.js?v=20260902_001',
   './assets/bookmarks.js?v=20260908_006',
