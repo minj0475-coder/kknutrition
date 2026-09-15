ca27b1858c1f51f9eaf4d8fb396933bb2f6f3bcc
minj0475-coder <minj0475@gmail.com>
2026-09-15 09:22:00 +0900

    feat: add nutrition quiz link to meal notes

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = 'kknutrition-pwa-v237';
+const CACHE_NAME = 'kknutrition-pwa-v238';
 const CORE_ASSETS = [
   './',
   './index.html',
   './school-lunch-tv.html?v=20260909_009',
-  './assets/site_v2.css?v=20260910_009',
+  './assets/site_v2.css?v=20260915_001',
   './assets/auth.js?v=20260710_001',
   './assets/data-guard.js?v=20260902_001',
   './assets/bookmarks.js?v=20260908_006',
